c3f919c8e5706821d0c9e2cafccb89f79f5772f9
pearl-truss <pearl@truss.works>
2022-12-19 20:18:26 -0500

    update text

--- src/components/drivers/unsupported-viewer.jsx
+++ src/components/drivers/unsupported-viewer.jsx
@@ -6,12 +6,8 @@ import 'styles/unsupported.scss';
 const UnsupportedViewer = props => (
   <div className="pg-driver-view">
     <div className="unsupported-message">
-      {props.unsupportedComponent
-        ? <props.unsupportedComponent {...props} />
-        : (
-          <p className="alert">{`No preview available for this kind of file.
-          Download file to see the contents.`}</p>
-        )}
+        <p>{`No preview available for this kind of file.
+        Download file to see the contents.`}</p>
     </div>
   </div>
 );

630d8b215944377aaa891fe0562e24ca1d8f7711
pearl-truss <pearl@truss.works>
2022-12-20 10:39:04 -0500

    format text in unsupported view

--- src/components/drivers/unsupported-viewer.jsx
+++ src/components/drivers/unsupported-viewer.jsx
@@ -6,8 +6,10 @@ import 'styles/unsupported.scss';
 const UnsupportedViewer = props => (
   <div className="pg-driver-view">
     <div className="unsupported-message">
-        <p>{`No preview available for this kind of file.
-        Download file to see the contents.`}</p>
+      <p>
+        {`No preview available for this kind of file.
+      Download file to see the contents.`}
+      </p>
     </div>
   </div>
 );

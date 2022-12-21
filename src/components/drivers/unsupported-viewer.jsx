c7ca98040fe814fdfea9ac390e9f377c75a6b5d2
pearl-truss <pearl@truss.works>
2022-12-21 15:15:57 -0500

    add line break to unsupported message

--- src/components/drivers/unsupported-viewer.jsx
+++ src/components/drivers/unsupported-viewer.jsx
@@ -1,20 +1,20 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
-import React from 'react';
-import 'styles/unsupported.scss';
+import React from 'react'
+import 'styles/unsupported.scss'
 
-const UnsupportedViewer = props => (
+const UnsupportedViewer = (props) => (
   <div className="pg-driver-view">
     <div className="unsupported-message">
-    {props.unsupportedComponent
-        ? <props.unsupportedComponent {...props} />
-        :
-      (<p>
-        {`No preview available for this kind of file.
-      Download file to see the contents.`}
-      </p>)}
+      {props.unsupportedComponent ? (
+        <props.unsupportedComponent {...props} />
+      ) : (
+        <p>
+          {`No preview available for this kind of file.\nDownload file to see the contents.`}
+        </p>
+      )}
     </div>
   </div>
-);
+)
 
-export default UnsupportedViewer;
+export default UnsupportedViewer

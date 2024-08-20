9805d88927273a87426ee9ec8a39aee056b6123d
AaronW <aaron.weatherford@caci.com>
2024-08-20 11:11:23 -0500

    corrected operator

--- src/components/drivers/pdf-viewer.jsx
+++ src/components/drivers/pdf-viewer.jsx
@@ -125,7 +125,7 @@ export default class PDFDriver extends React.Component {
 
   componentWillUnmount() {
     // Only utilize PDFJS.getDocument() if isEvalSupported == false.
-    if (PDFJS.isEvalSupported == false) {
+    if (PDFJS.isEvalSupported === false) {
       const { pdf } = this.state
       if (pdf) {
         pdf.destroy()
@@ -194,22 +194,19 @@ export default class PDFDriver extends React.Component {
             <button
               type="button"
               className="view-control"
-              onClick={this.increaseZoom}
-            >
+              onClick={this.increaseZoom}>
               <i className="zoom-in" />
             </button>
             <button
               type="button"
               className="view-control"
-              onClick={this.resetZoom}
-            >
+              onClick={this.resetZoom}>
               <i className="zoom-reset" />
             </button>
             <button
               type="button"
               className="view-control"
-              onClick={this.reduceZoom}
-            >
+              onClick={this.reduceZoom}>
               <i className="zoom-out" />
             </button>
           </div>

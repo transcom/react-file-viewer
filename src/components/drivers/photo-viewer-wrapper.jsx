0949c1cda026dd962cef4d9d6f57f0e11d52a446
Steven Gleason <180579696+stevengleason-caci@users.noreply.github.com>
2024-10-09 17:35:21 -0500

    B-21049 use provided onError
    instead of just logging image errors in photo-viewer-wrapper.

--- src/components/drivers/photo-viewer-wrapper.jsx
+++ src/components/drivers/photo-viewer-wrapper.jsx
@@ -29,11 +29,18 @@ export default class PhotoViewerWrapper extends Component {
     // spike on using promises and a different loader or adding three js loading manager
     const loader = new THREE.TextureLoader();
     loader.crossOrigin = '';
+    // get error handler
+    var onError = this.props.onError;
+    if (typeof (onError) == 'undefined' || onError == null) {
+      onError = (xhr) => {
+        console.log('An error happened', xhr);
+      };
+    }
     // load a resource
     loader.load(
       // resource URL
       this.props.filePath,
-      // Function when resource is   loaded
+      // Function when resource is loaded
       (texture) => {
         this.setState({
           originalWidth: texture.image.width,
@@ -45,9 +52,7 @@ export default class PhotoViewerWrapper extends Component {
       (xhr) => {
         console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
       },
-      (xhr) => {
-        console.log('An error happened', xhr);
-      },
+      onError,
     );
   }
 

fcf85560b6855aa7061382c2fdee86946101f3b2
Steven Gleason <180579696+stevengleason-caci@users.noreply.github.com>
2024-10-21 13:35:18 -0500

    B-21049 unit tests for pdf-viewer onError.

--- tests/components/pdf-viewer.test.js
+++ tests/components/pdf-viewer.test.js
@@ -2,20 +2,22 @@
 import { readFileSync } from 'fs'
 
 import React from 'react';
+import { PDFJS } from 'pdfjs-dist/build/pdf.combined'
 import { mount } from 'enzyme';
 import { createWaitForElement } from 'enzyme-wait';
 import { PDFPage } from 'components/drivers/pdf-viewer';
 import PDFDriver from '../../src/components/drivers/pdf-viewer'
 
 describe('pdf-viewer', () => {
-  let spy;
+  let spyFetchAndRender, spyPdfjsGetDocument;
   beforeEach(() => {
-    spy = jest.spyOn(PDFPage.prototype, 'fetchAndRenderPage').mockImplementation(() => {})
+    spyFetchAndRender = jest.spyOn(PDFPage.prototype, 'fetchAndRenderPage').mockImplementation(() => {});
+    spyPdfjsGetDocument = jest.spyOn(PDFJS, 'getDocument');
   })
 
   afterEach(() => {
-    spy.mockReset();
-    spy.mockRestore();
+    spyFetchAndRender.mockRestore();
+    spyPdfjsGetDocument.mockRestore();
   })
 
   it('renders without crashing', () => {
@@ -28,14 +30,14 @@ describe('pdf-viewer', () => {
    mount(
       <PDFPage fileType='fake' filePath='fake/path' disableVisibilityCheck />
     );
-    expect(spy).toHaveBeenCalled();
+    expect(spyFetchAndRender).toHaveBeenCalled();
   });
 
   it('does not call fetchAndRenderPage on mount with visibility check enabled', () => {
     mount(
       <PDFPage fileType='fake' filePath='fake/path' disableVisibilityCheck={false} />
     );
-    expect(spy).not.toHaveBeenCalled();
+    expect(spyFetchAndRender).not.toHaveBeenCalled();
   });
 
   it('updates loading progress state', async () => {
@@ -47,4 +49,26 @@ describe('pdf-viewer', () => {
       expect(componentReady.state().percent).toBe('100')
     })
   });
+
+  it('does not call onError for a successful file', async () => {
+    function onError(error) {
+      throw Error('onError should not be called');
+    };
+    const fileContents = readFileSync('./example_files/sample.pdf');
+    mount(
+      <PDFDriver fileType='pdf' filePath={fileContents} onError={onError} />
+    );
+  });
+
+  it('does call onError for a missing file', async () => {
+    function mockGetDocument(path) {
+      return Promise.reject(Error('the world is not enough'));
+    }
+    spyPdfjsGetDocument.mockImplementation(mockGetDocument);
+    const mockErrorHandler = jest.fn();
+    mount(
+      <PDFDriver fileType='pdf' filePath='./example_files/missing_file.pdf' onError={mockErrorHandler} />
+    );
+  });
+
 });

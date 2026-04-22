9f5efb1456090a1d02fca2adba7fa2704600e0c0
Alex Lusk <alex.lusk@caci.com>
2026-04-22 18:20:21 -0400

    loading test

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,13 +1,14 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
-import React from 'react';
+import React from 'react'
 
-import 'styles/loading.scss';
+import 'styles/loading.scss'
 
 const Loading = () => (
   <div className="loading-container">
     <span className="loading" />
+    LOADING!!!
   </div>
-);
+)
 
-export default Loading;
+export default Loading
